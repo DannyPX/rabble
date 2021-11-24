f5dc10e895d2615a518432dd98081ef253a36c6f
JayVnl <jorrit.v@outlook.com>
2021-11-24 10:48:18 +0100

    Added constants

---
+++ lib/constants.dart
@@ -0,0 +1,104 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color(0x131521FF);
+
+// TODO Gradient
+const kPrimaryColor = Color(0x783E88FF);
+const kSecondaryColor = Color(0xF86F67FF);
+
+const kTextPrimaryColor = Color(0xFFFFFFFF);
+const kTextSecondaryColor = Color(0xB1B2D1FF);
+const kTextTertiaryColor = Color(0x7C7B9FFF);
+
+// TODO Gradient
+const kButtonHighlightedPrimaryColor = Color(0x2D2F3CFF);
+const kButtonHighlightedSecondaryColor = Color(0x363749FF);
+const kButtonBackgroundColor = Color(0x1F212EFF);
+
+// Text Styles
+var kLargeTitleStyle = TextStyle(
+  fontSize: 28.0,
+  fontWeight: FontWeight.bold,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
+var kTitle1Style = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
+var kCardTitleStyle = TextStyle(
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  fontWeight: FontWeight.bold,
+  color: kTextPrimaryColor,
+  fontSize: 22.0,
+  decoration: TextDecoration.none,
+);
+var kTitle2Style = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
+var kHeadlineLabelStyle = TextStyle(
+  fontSize: 17.0,
+  fontWeight: FontWeight.w800,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
+var kSubtitleStyle = TextStyle(
+  fontSize: 16.0,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
+var kBodyLabelStyle = TextStyle(
+  fontSize: 16.0,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
+var kCalloutLabelStyle = TextStyle(
+  fontSize: 16.0,
+  fontWeight: FontWeight.w800,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
+var kSecondaryCalloutLabelStyle = TextStyle(
+  fontSize: 16.0,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
+var kSearchPlaceholderStyle = TextStyle(
+  fontSize: 13.0,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
+var kSearchTextStyle = TextStyle(
+  fontSize: 13.0,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
+var kCardSubtitleStyle = TextStyle(
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  color: kTextPrimaryColor,
+  fontSize: 13.0,
+  decoration: TextDecoration.none,
+);
+var kCaptionLabelStyle = TextStyle(
+  fontSize: 12.0,
+  color: kTextPrimaryColor,
+  fontFamily: Platform.isIOS ? 'SF Pro Text' : null,
+  decoration: TextDecoration.none,
+);
