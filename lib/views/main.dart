0d4e85b3a2b0f3ced5865556ca36d22473c5d51c
JayVnl <jorrit.v@outlook.com>
2021-11-26 10:17:48 +0100

    Navbar transparency fix

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -30,34 +30,39 @@ class _HomePageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       backgroundColor: cBackgroundColor,
-      // MADE THIS A STACK FOR CURRENTLY PLAYING CARD ADDED LATER
       body: Stack(
         children: [
           SingleChildScrollView(
             child: SafeArea(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                // padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                padding: const EdgeInsets.only(
+                    left: 20.0, right: 20.0, bottom: 83.0),
                 child: _widgetOptions.elementAt(_selectedIndex),
               ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: const [
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Player(
-                  title: 'Stromae - Santé (Official Music Video)',
-                  subtitle: 'Stromae',
-                  imageUrl: 'assets/images/onboarding.jpg',
-                  totalTime: Duration(minutes: 3),
-                  currentTime: Duration(minutes: 2, seconds: 30),
+          Container(
+            margin: const EdgeInsets.only(bottom: 50.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Player(
+                    title: 'Stromae - Santé (Official Music Video)',
+                    subtitle: 'Stromae',
+                    imageUrl: 'assets/images/onboarding.jpg',
+                    totalTime: Duration(minutes: 3),
+                    currentTime: Duration(minutes: 2, seconds: 30),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
